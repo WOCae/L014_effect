54d830679c542f1aa45f263095951fd1e8794925
WOCae <t-satoIZ@outlook.jp>
2024-03-01 21:34:10 +0900

    Update exp.gd

--- exp.gd
+++ exp.gd
@@ -1,21 +1,16 @@
 extends Sprite2D
 
-# 演出速度.
-const SPEED = 34*2 # 37パターンで1秒,
-
-# 経過時間.
-var _pasttime = 0.0
+const SPEED = 100 #エフェクト速さ
+var passTime = 0.0 #時間計測用変数
 
 func _ready() -> void:
 	#position = Vector2(100, 100)
 	scale =  Vector2(5,5)	#サイズ
 	
-## 更新.
 func _process(delta: float) -> void:
 	# 経過時間を足し込む.
-	_pasttime += delta
-	
-	frame = int(_pasttime * SPEED)
-	if frame >= 30:
+	passTime += delta
+	frame = int(passTime * SPEED)
+	if frame >= 33:
 		# アニメーションが終わったら消す.
 		queue_free()
